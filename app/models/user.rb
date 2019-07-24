3b6c38c432aff5b0b26703b646e952122615c120
Wellstone515 <info.fullmoons@gmail.com>
2019-07-24 16:20:54 +0900

    Create model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :comments
+  has_many :liked_products, through: :likes, source: :product
+  has_one :credit
+  has_one :address
 end
