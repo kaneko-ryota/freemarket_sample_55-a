ce16c011b3cd499d39f3ecfbde252ad2d2b89eba
Wellstone515 <info.fullmoons@gmail.com>
2019-07-24 16:21:04 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'products/index'
   root to: 'products#index'
+  # resources :products
+  resources :products do
+    resources :comments
+    resources :likes, only: [:create, :destroy]
+  end
+  resources :addresses
+  resources :credits
+  resources :product_images
+  resources :brands
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
