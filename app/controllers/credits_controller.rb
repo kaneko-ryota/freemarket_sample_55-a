d5ce8aec5e4114507c7bd4e109f175141f45bf0b
sato <fri3uspqr@gmail.com>
2019-08-05 15:59:28 +0900

    カード情報登録（サインアップ）
    サインアップでのカード情報登録ができるように追加※singupコントローラーのbefore_actionをコメントアウトしています。//ユーザーページでカード登録の遷移のためのリンク設定をした

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -3,13 +3,14 @@ class CreditsController < ApplicationController
   require "payjp"
   
   def index
-
   end
   
   def new
+    @credit = Credit.new
+    @credit.save
   end
 
-  def create    
+  def create
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     customer = Payjp::Customer.create
     card = customer.cards.create(card: params[:payjp_Token])
@@ -17,7 +18,8 @@ class CreditsController < ApplicationController
       user_id:     current_user.id,
       customer_id: customer.id,
       card_id:     card.id
-      )
+    )
+    redirect_to user_path(current_user)
   end
 
   def edit
--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -3,7 +3,7 @@ crumb :root do
 end
 
 crumb :mypage do |user|
-  link "マイページ", user_path(current_user)
+  # link "マイページ", user_path(current_user)
 end
 
 crumb :detail do |product|
