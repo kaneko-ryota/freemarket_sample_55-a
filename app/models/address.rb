0c2a067a3a86a27ae5df3c750340e31ffb73c036
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-08-12 14:37:33 +0900

    Update address.rb
    user_id一つにつき一つの住所のみ登録できるようにしました。

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,7 @@ class Address < ApplicationRecord
   delegate :name, to: :prefecture
   belongs_to :user
 
-  # validates_uniquness_of :user_id
+  validates :user_id, uniqueness: true
   validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
   validates :prefecture_id, presence: true
   validates :city, presence: true
