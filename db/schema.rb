cda5694ff8f4b0566c144f061fdb16487bdcd607
kaneko-ryota <ryota1611292@icloud.com>
2019-08-13 15:20:17 +0900

    add:flash
    #what
    プロフィール編集後のフラッシュ
    back
    Nike
    ＃why
    変更したかわかりやすく
    redcameraにも戻るボタン追加
    NIKEのとこに画像だけレディースがいた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190805055955) do
+ActiveRecord::Schema.define(version: 20190812041316) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",       null: false
