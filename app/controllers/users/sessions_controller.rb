7ddd1fb7e61e706b3cbdc0cf70bb9a59de7cdda6
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-08-13 20:57:33 +0900

    delete session flash message
    ログインした時に強制的に表示されるフラッシュメッセージを削除しました。

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   prepend_before_action :check_captcha, only: [:create]
+  after_filter :delete_flash, only: [:create, :destory]
 
 
   def new
@@ -20,6 +21,13 @@ class Users::SessionsController < Devise::SessionsController
       self.resource = resource_class.new
       resource.validate
       render :new
+    end
+  end
+  
+  protected
+  def delete_flash
+    if flash[:notice].present?
+      flash.delete(:notice)
     end
   end
 end
