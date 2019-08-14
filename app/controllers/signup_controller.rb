352f2f5413e43f0a116c5eb618ccb32e080ea61c
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-08-14 13:29:23 +0900

    Update signup_controller.rb
    電話番号認証をコメントアウトしました。

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -60,13 +60,13 @@ class SignupController < ApplicationController
     @user_phone_number = "+81#{user_phone_params[:phone_number][1..-1]}"
 
     # MEMO: SMSを送信できる回数/電話番号が限られているのでコメントアウトしてあります
-    client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
-    client.messages.create(
-      from: ENV['TWILIO_NUMBER'],
-      to: @user_phone_number,
-      body: @@generate_token
-    )
-  end
+  #   client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
+  #   client.messages.create(
+  #     from: ENV['TWILIO_NUMBER'],
+  #     to: @user_phone_number,
+  #     body: @@generate_token
+  #   )
+  # end
 
 
   def user_info
